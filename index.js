d60571b81b13384a582146f2279e4b01d61bab66
Mike Borowy <mikeborowy@gmail.com>
2018-09-29 12:07:32 +0200

    added new key

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-
+//register modules
 require('./models/User');
 require('./models/Blog');
-require('./services/passport');
-
+require('./services/passport');//auth
+//setup mongose instance 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
-
+//create express app
 const app = express();
-
+//settup middleware
 app.use(bodyParser.json());
 app.use(
   cookieSession({
@@ -23,10 +23,10 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
+//register event handlers
 require('./routes/authRoutes')(app);
 require('./routes/blogRoutes')(app);
-
+//setup prodouction file serving
 if (['production'].includes(process.env.NODE_ENV)) {
   app.use(express.static('client/build'));
 
@@ -35,7 +35,7 @@ if (['production'].includes(process.env.NODE_ENV)) {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
-
+//app start listening
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
